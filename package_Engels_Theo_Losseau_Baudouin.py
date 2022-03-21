2736b6cd838b5b091164b19bb988dbf160f4912d
olingo99 <engels.theo@skynet.be>
2022-03-21 12:35:18 +0100

    pid

--- package_Engels_Theo_Losseau_Baudouin.py
+++ package_Engels_Theo_Losseau_Baudouin.py
@@ -19,3 +19,32 @@ def Lead_Lag_Discreet_RT(MV,PV,Tlead,Tlag,Ts,Kp=1,method='EBD',PVInit = 0):
         else:
             PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K))*MV[-1])
             
+def PID_RT(SP,PV,Man,MVMan,MVFF,Kc,Ti,Td,Ts,MVMin,MVMax,MV,MVP,MVI,MVD,E,alpha = 0.4,ManFF=False,PVInit = 0,method = "EBD-EBD"):
+    Tfd = alpha*Td
+    
+    if Man:
+        pass
+    if len(MVI) == 0 or len(MVD)==0 or len(MVP)==0:
+        if len(MVI) == 0:    
+            MVI.append(PVInit)
+        if len(MVD) == 0:
+            MVD.append(PVInit)
+        if len(MVP) == 0:
+            MVP.append(PVInit)
+        print("ici")
+    else:
+        print("la")
+        e  = (SP[-1]-PV[-1])
+        E.append(e)
+        temp_mvp = Kc*e
+        MVP.append(temp_mvp)
+        temp_mvi = MVI[-1]+Kc*(Ts/Ti)*e
+        temp_mvd = (Tfd/(Tfd+Ts))*MVD[-1]+(Kc*Td/(Tfd+Ts))*(e-E[-2])
+        MVD.append(temp_mvd)
+        if temp_mvp+temp_mvi+temp_mvd>MVMax:  
+            MVI.append(MVMax-temp_mvp-temp_mvd)
+        elif temp_mvp+temp_mvi+temp_mvd<MVMin:
+            MVI.append(MVMin-temp_mvp-temp_mvd)
+        else:
+            MVI.append(temp_mvi)
+        MV.append(MVP[-1]+MVI[-1]+MVD[-1])
